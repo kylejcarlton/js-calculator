0b8e41c28697164571667e653556049e44ffe67a
Kyle J Carlton <kylejcarlton@gmail.com>
2017-11-05 12:56:38 -0600

    basic logic in script.js
    also ad id to each button

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
+  var calculation = [];
+  var operators ={plus:"+", minus:"-", multiply:"*", divide:"/"};
+
+  $("ul.numbers li").click(function(){
+    calculation.push($(this).attr("id"));
+    console.log(calculation);
+  });
+  $("ul.operators li").click(function(){
+      calculation.push(operators[$(this).attr("id")]);
+      console.log(calculation);
+  });
+  $("ul.actions li").click(function(){
+      if($(this).attr("id") == "ce"){
+        calculation.pop();
+        console.log(calculation);
+      }
+      if($(this).attr("id") == "c"){
+        calculation = [];
+      }
+      if($(this).attr("id") == "equals"){
+      calculation = calculation.join(" ");
+      console.log(eval(calculation));
+      //https://stackoverflow.com/questions/5834318/are-variable-operators-possible
+
+      }
+  });
+
 
 }); //END DOC READY JS
