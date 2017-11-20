3d49f765de9b2b873531bd3d8cbbdf1679e9ef7e
Kyle J Carlton <kylejcarlton@gmail.com>
2017-11-19 18:43:21 -0600

    write values entered and results to display section of UI

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
   var calculation = [];
-  var operators ={plus:"+", minus:"-", multiply:"*", divide:"/"};
+  var operators ={plus:"+", minus:"-", multiply:"*", divide:"/", equals:" ="};
 
   $("ul.numbers li").click(function(){
     calculation.push($(this).attr("id"));
-    console.log(calculation);
+    $("#small").html(calculation.join(""));
   });
   $("ul.operators li").click(function(){
       calculation.push(operators[$(this).attr("id")]);
-      console.log(calculation);
+      $("#small").html(calculation.join(""));
   });
   $("ul.actions li").click(function(){
       if($(this).attr("id") == "ce"){
         calculation.pop();
-        console.log(calculation);
+        $("#small").html(calculation.join(""));
       }
       if($(this).attr("id") == "c"){
         calculation = [];
       }
       if($(this).attr("id") == "equals"){
-      calculation = calculation.join(" ");
-      console.log(eval(calculation));
+      $("#large").html(eval(calculation.join("")));
+      calculation.push(operators[$(this).attr("id")]);
+      $("#small").html(calculation.join(""));
       //https://stackoverflow.com/questions/5834318/are-variable-operators-possible
 
       }
