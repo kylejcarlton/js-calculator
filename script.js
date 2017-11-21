5d31e4177bbbf60db912f4eb3eac40d23715eb8c
Kyle J Carlton <kylejcarlton@gmail.com>
2017-11-21 11:39:01 -0600

    adjust for decimal point and round eval to precision 3

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
   var calculation = [];
-  var operators ={plus:"+", minus:"-", multiply:"*", divide:"/"};
+  var operators ={plus:"+", minus:"-", multiply:"*", divide:"/", decimal:"."};
 
   $("ul.numbers li").click(function(){
-    calculation.push($(this).attr("id"));
+    //console.log(typeof($(this).attr("id")));
+    //console.log(operators[$(this).attr("id")]);
+    if($(this).attr("id") == 'decimal'){
+      calculation.push(operators[$(this).attr("id")]);
+    }
+    else{
+      calculation.push($(this).attr("id"));
+    }
     $("#small").html(calculation.join(""));
   });
   $("ul.operators li").click(function(){
@@ -21,7 +28,8 @@ $(document).ready(function(){
         $("#small").html("");
       }
       if($(this).attr("id") == "equals"){
-      $("#large").html(Math.round(eval(calculation.join(""))), 2);
+      console.log(typeof(eval(calculation.join(""))));
+      $("#large").html(eval(calculation.join("")).toPrecision(3));
       $("#small").html(calculation.join("")+" =");
       calculation = [];
       //https://stackoverflow.com/questions/5834318/are-variable-operators-possible
