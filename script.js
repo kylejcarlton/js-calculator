48edebefa38b66873f438ccba45e845daa1589bf
Kyle J Carlton <kylejcarlton@gmail.com>
2017-11-21 12:11:44 -0600

    add function to check for max character length in calculation [

--- script.js
+++ script.js
@@ -2,25 +2,38 @@ $(document).ready(function(){
   var calculation = [];
   var operators ={plus:"+", minus:"-", multiply:"*", divide:"/", decimal:"."};
 
+  // https://stackoverflow.com/questions/1055767/why-can-i-not-define-functions-in-jquerys-document-ready
+  var maxLengthCheck = function(){
+    if(calculation.length < 26){
+      $("#small").html(calculation.join(""));
+    }
+    else{
+      $("#large").html(eval(calculation.join("")).toPrecision(3));
+      $("#small").html(calculation.join("")+" =");
+      calculation = [];
+    }
+  }
+
   $("ul.numbers li").click(function(){
-    //console.log(typeof($(this).attr("id")));
-    //console.log(operators[$(this).attr("id")]);
     if($(this).attr("id") == 'decimal'){
       calculation.push(operators[$(this).attr("id")]);
     }
     else{
       calculation.push($(this).attr("id"));
     }
-    $("#small").html(calculation.join(""));
+      //$("#small").html(calculation.join(""));
+      maxLengthCheck();
   });
   $("ul.operators li").click(function(){
       calculation.push(operators[$(this).attr("id")]);
-      $("#small").html(calculation.join(""));
+      //$("#small").html(calculation.join(""));
+      maxLengthCheck();
   });
   $("ul.actions li").click(function(){
       if($(this).attr("id") == "ce"){
         calculation.pop();
-        $("#small").html(calculation.join(""));
+        //$("#small").html(calculation.join(""));
+        maxLengthCheck();
       }
       if($(this).attr("id") == "c"){
         calculation = [];
@@ -28,7 +41,6 @@ $(document).ready(function(){
         $("#small").html("");
       }
       if($(this).attr("id") == "equals"){
-      console.log(typeof(eval(calculation.join(""))));
       $("#large").html(eval(calculation.join("")).toPrecision(3));
       $("#small").html(calculation.join("")+" =");
       calculation = [];
